49daed7661d1438962aedb5c2b37889c9389a922
Nicolás Rubio <nic6131@hotmail.com>
2025-01-26 21:38:21 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/pruebaexplora",
+  // basePath: "/pruebaexplora",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
